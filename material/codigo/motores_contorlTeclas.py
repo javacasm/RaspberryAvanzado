6667f011fa28ac683e7bb92d2e21359c40d6f10b
JAVacasm <javacasm@gmail.com>
2018-03-11 22:45:47 +0100

    mv

--- material/codigo/motores_contorlTeclas.py
+++ material/codigo/motores_contorlTeclas.py
@@ -15,6 +15,10 @@ def getch():
 
 robot = Robot(left = (7, 8), right = (9, 10))
 
+speed = 0.4
+
+print("Usar:\n\tw forward\n\ts stop\n\td leftt\n\ta right\n\tz backward\n\t1,5,9  speed\n\tq quit\n")
+
 while True:
   # Keyboard character retrieval method is called and saved
   # into variable
@@ -23,25 +27,44 @@ while True:
 
   # The car will drive forward when the "w" key is pressed
   if(char == "w"):
-       robot.forward(1)
+       robot.forward(speed)
 
-  # The car will reverse when the "s" key is pressed
-  if(char == "s"):
-      robot.right(0.4)
+  # The car will reverse when the "z" key is pressed
+  if(char == "z"):
+      robot.backward(speed)
 
   # The "a" key will toggle the steering left
   if(char == "a"):
-      robot.right(0.4)
+      robot.left(speed)
 
   # The "d" key will toggle the steering right
   if(char == "d"):
-      toggleSteering("right")
+      robot.right(speed)
+
+  # The "s" key will  stop the car
+  if(char == "s"):
+      robot.stop()
+
+  # The "1" key will toggle  lowest speed
+  if(char == "1"):
+      speed = 0.1
+
+  # The "5" key will toggle  medium speed
+  if(char == "5"):
+      speed = 0.5
+
+  # The "9" key will toggle  hiwest speed
+  if(char == "9"):
+      speed = 0.9
+
+
+
 
   # The "l" key will toggle the LEDs on/off
   if(char == "l"):
       toggleLights()
 
   # The "x" key will break the loop and exit the program
-  if(char == "x"):
+  if(char == "q"):
       print("Program Ended")
       break
