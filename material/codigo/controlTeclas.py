62807aca9759dc33f6757f47599c405e3df24633
javacasm <javacasm@gmail.com>
2018-03-11 20:43:27 +0100

    formato

--- material/codigo/controlTeclas.py
+++ material/codigo/controlTeclas.py
@@ -1,48 +1,48 @@
-Control con teclas
+# Control del robot con teclas
 
-  import getch
-  import sys, tty, termios, time
-  from gpiozero import Robot
+import getch
+import sys, tty, termios, time
+from gpiozero import Robot
 
-  def getch():
-      fd = sys.stdin.fileno()
-      old_settings = termios.tcgetattr(fd)
-      try:
-          tty.setraw(sys.stdin.fileno())
-          ch = sys.stdin.read(1)
-      finally:
-          termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-      return ch
+def getch():
+  fd = sys.stdin.fileno()
+  old_settings = termios.tcgetattr(fd)
+  try:
+      tty.setraw(sys.stdin.fileno())
+      ch = sys.stdin.read(1)
+  finally:
+      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+  return ch
 
-  robot = Robot(left = (7, 8), right = (9, 10))
+robot = Robot(left = (7, 8), right = (9, 10))
 
-  while True:
-      # Keyboard character retrieval method is called and saved
-      # into variable
+while True:
+  # Keyboard character retrieval method is called and saved
+  # into variable
 
-      char = getch()
+  char = getch()
 
-      # The car will drive forward when the "w" key is pressed
-      if(char == "w"):
-           robot.forward(1)
+  # The car will drive forward when the "w" key is pressed
+  if(char == "w"):
+       robot.forward(1)
 
-      # The car will reverse when the "s" key is pressed
-      if(char == "s"):
-          robot.right(0.4)
+  # The car will reverse when the "s" key is pressed
+  if(char == "s"):
+      robot.right(0.4)
 
-      # The "a" key will toggle the steering left
-      if(char == "a"):
-          robot.right(0.4)
+  # The "a" key will toggle the steering left
+  if(char == "a"):
+      robot.right(0.4)
 
-      # The "d" key will toggle the steering right
-      if(char == "d"):
-          toggleSteering("right")
+  # The "d" key will toggle the steering right
+  if(char == "d"):
+      toggleSteering("right")
 
-      # The "l" key will toggle the LEDs on/off
-      if(char == "l"):
-          toggleLights()
+  # The "l" key will toggle the LEDs on/off
+  if(char == "l"):
+      toggleLights()
 
-      # The "x" key will break the loop and exit the program
-      if(char == "x"):
-          print("Program Ended")
-          break
+  # The "x" key will break the loop and exit the program
+  if(char == "x"):
+      print("Program Ended")
+      break
