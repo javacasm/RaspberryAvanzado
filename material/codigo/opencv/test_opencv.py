489ff69bcbec52b64cd1b3ccfffad04382a0e876
Javacasm <javacasm@gmail.com>
2018-03-13 13:22:19 +0100

    update

--- material/codigo/opencv/test_opencv.py
+++ material/codigo/opencv/test_opencv.py
@@ -2,19 +2,25 @@ import os
 import cv2
 import math
 
-## Capturamos la imagen con la cámara de la Raspberry Pi
+# Usamos el metodo resize para re-escalar
+def resizeImage(img):
+        dst = cv2.resize(img,None, fx=0.25, fy=0.25, interpolation =
+        cv2.INTER_LINEAR)
+        return dst
+
+## Capturamos la imagen con la camara de la Raspberry Pi
 os.system("raspistill -o image.jpg")
 ## Cargamos la imagen
-img = cv2.imread("/home/pi/Desktop/image.jpg")
-grey = cv2.imread("/home/pi/Desktop/image.jpg",0) #0 para la escala de grises
+img = cv2.imread("./image.jpg")
+grey = cv2.imread("./image.jpg",0) #0 para la escala de grises
 
-
+cv2.imshow("img",img)
+cv2.imshow("grey",grey)
 ## convertimos la imagen en grises en una en blanco y negro
 ret, thresh = cv2.threshold(grey,50,255,cv2.THRESH_BINARY)
-
-## Re-escalamos las imágenes
+## Re-escalamos las imagenes
 img = resizeImage(img)
 thresh = resizeImage(thresh)
 ## las mostramos en pantalla
 cv2.imshow("thresh",thresh)
-cv2.imshow("img",img)
+
